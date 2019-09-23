29b644d5f5ec12e767367bd34df195d5f5999ec4
poplar <poplar@qq.com>
2019-09-23 22:03:24 +0800

    修改编码及重写ChannelInboundHandlerAdapter类的方法，测试执行流程

--- src/main/java/cpm/poplar/hello/HelloWorldTestHandler.java
+++ src/main/java/cpm/poplar/hello/HelloWorldTestHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  * Created By poplar on 2019/9/22
  */
@@ -15,6 +17,13 @@ public class HelloWorldTestHandler extends SimpleChannelInboundHandler<HttpObjec
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
         if (msg instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest) msg;
+            System.out.println("请求方法名： " + request.method().name());
+            URI uri = new URI(request.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("/favicon.ico");
+                return;
+            }
             //响应会客户端的内容
             ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
@@ -23,5 +32,35 @@ public class HelloWorldTestHandler extends SimpleChannelInboundHandler<HttpObjec
             //把内容写到客服端
             ctx.writeAndFlush(response);
         }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered");
+        super.channelUnregistered(ctx);
     }
 }
